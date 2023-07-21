c0e26655f1761fcb115ef9796321c43e3bdaec30
mzdyhrave <info@hravemzdy.org>
2023-07-21 14:14:58 +0200

    Add project files.

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace mzdyhrave_create_pages_with_mudblazor_net8
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
