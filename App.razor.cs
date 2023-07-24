42dd3195610ef4ce258237d7a0d08eeb7c75b0ec
mzdyhrave <info@hravemzdy.org>
2023-07-24 16:26:41 +0200

    initial version

---
+++ App.razor.cs
@@ -0,0 +1,23 @@
+﻿using MudBlazor;
+
+namespace EmployeeManagementApp;
+
+public partial class App
+{
+    private MudTheme EmployeeManagementTheme { get; init; }
+
+    public App()
+    {
+        EmployeeManagementTheme = new MudTheme()
+        {
+            // https://imagecolorpicker.com/color-code/61b551
+
+            Palette = new PaletteLight()
+            {
+                AppbarText = new MudBlazor.Utilities.MudColor(255, 255, 255, 1),
+                AppbarBackground = new MudBlazor.Utilities.MudColor(97, 181, 81, 255),
+                Primary = new MudBlazor.Utilities.MudColor(97, 181, 81, 255),
+            },
+        };
+    }
+}
